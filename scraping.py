6487d48ee7a9b47c96ed2cb159d1da40987809f0
Patricio Brito <patricio@drinkfitzer.com>
2022-09-05 19:21:24 -0500

    challenge updated

--- scraping.py
+++ scraping.py
@@ -18,6 +18,7 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
+        "hemispheres": hemispheres(browser),
         "last_modified": dt.datetime.now()
     }
 
@@ -97,6 +98,34 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
 
+def hemispheres(browser):
+    
+
+        # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+
+    browser.visit(url)
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    links = browser.find_by_css("a.product-item img")
+    for item in range (len(links)):
+        hemisphere={}
+        
+        browser.find_by_css("a.product-item img")[item].click()
+
+        sample = browser.links.find_by_text("Sample").first
+        hemisphere["img_url"]=sample["href"]
+        
+        hemisphere["title"]=browser.find_by_css("h2.title").text
+        
+        hemisphere_image_urls.append(hemisphere)
+        
+        browser.back()
+
+    return hemisphere_image_urls
+    
 if __name__ == "__main__":
 
     # If running as script, print scraped data
